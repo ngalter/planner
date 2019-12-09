cf84e18e05384f71d348f0e03c6724993ca731bf
Nancy Alter <nancy.alter@gmail.com>
2019-12-09 14:24:17 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// A $( document ).ready() block.
+    $( document ).ready(function() {
+    var currentHour = moment().format("HH");
+    var newHour = currentHour;
+    startTimer();
+    createCalendar(); 
+    checkHours();
+
+
+
+    function startTimer()
+    {
+        var d = moment().format("dddd, MMMM Do, YYYY");
+        $("#currentDay").html(d);
+        console.log(currentHour);
+
+        var myTimer = setInterval(function()
+        {
+            newHour = moment().format("HH");
+            if (currentHour !== newHour)
+            {
+                currentHour = newHour;
+                console.log(currentHour);
+            };
+        },1000);   
+    }
+
+    function createCalendar()
+    {
+        var myHour = ["9am","10am","11am","Noon","1pm","2pm","3pm","4pm","5pm"];
+        var hours = ["09","10","11","12","13","14","15","16","17"];
+        var $timeBlock = $("#timeBlock");
+        $.each(myHour, function (i, value) 
+        {
+            console.log(value); 
+            var strI = i.toString();
+            var calHour = $("<p>").text(value).addClass("hour");
+            var calInput = $("<textarea rows='2'>").addClass ("past time-block").attr("id",hours[i]);
+            var calBtn = $("<button>").html("<i class=\"fas fa-download\"></i>").addClass   ("saveBtn");
+            $("#timeBlock").append(calHour,calInput,calBtn);
+        });
+    }
+
+
+    function checkHours()
+    {  
+            console.log(currentHour);
+            var myId = document.getElementById(currentHour);
+            console.log(myId);
+                $(myId).addClass("present");
+                $(myId).removeClass("past");
+                $(myId).removeClass("future");	
+                $(myId).nextAll("textarea").addClass( "future");
+                $(myId).nextAll("textarea").removeClass( "past present");
+                $(myId).prevAll("textarea").addClass( "past");
+                $(myId).prevAll("textarea").removeClass( "present future");
+    }
+});
